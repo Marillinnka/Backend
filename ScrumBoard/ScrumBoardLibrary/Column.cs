c4dd5c236a284ef05147d2cea7335b1e7d08637d
Makarova Marina <marina25032002@mail.ru>
2022-04-04 21:44:24 +0300

    Добавила проект Scrum board

---
+++ ScrumBoard/ScrumBoardLibrary/Column.cs
@@ -0,0 +1,64 @@
+﻿
+namespace ScrumBoardLibrary
+{
+    public class Column : IColumn
+    {
+        string ExistTask = "You cannot add an existing task";
+        public string UnicalID { get; }
+        public string ColumnName { get; set; }
+
+        private readonly List<Task> Tasks;
+
+        public Column(string name)
+        {
+            ColumnName = name;
+            UnicalID = Guid.NewGuid().ToString();
+            Tasks = new List<Task>();
+        }
+
+        public void AddTask(Task task)
+        {
+            if(Tasks.Contains(task))
+            {
+                throw new Exception(ExistTask);
+            }
+
+            else
+                Tasks.Add(task);
+        }
+
+        public bool DeleteTask(string taskUnicalID)
+        {
+            int tasksListSize = Tasks.Count;
+
+            for (int i = 0; i < tasksListSize; i++)
+            {
+                if (taskUnicalID == Tasks[i].UnicalID)
+                {
+                    Tasks.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Task? GetTask(string taskUnicalID)
+        {
+            int tasksListSize = Tasks.Count;
+
+            for (int i = 0; i < tasksListSize; i++)
+            {
+                if (taskUnicalID == Tasks[i].UnicalID)
+                {
+                    return Tasks[i];
+                }
+            }
+            return null;
+        }
+
+        public List<Task> LookAllTasks()
+        {
+            return Tasks;
+        }
+    }
+}
---
+++ ScrumBoard/ScrumBoardLibrary/IBoard.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ScrumBoardLibrary
+{
+    internal interface IBoard
+    {
+        public string BoardName { get; }
+        public void AddTaskIntoColumn(Task task, int columnNum = 0);
+        public void MoveTaskBetweenColumns(string columnUnicalID, string  taskUnicalID);
+        public Task GetTask(string taskUnicalID);
+        public void DeleteTask(string unicalID);
+
+        public void AddColumn(Column column);
+        public Column GetColumn(string columnUnicalID);
+        public List<Column> LookAllColumn();
+
+
+    }
+}
